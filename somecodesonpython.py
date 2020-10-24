aeebf563b6c1c7aee055f546bbd989de594bf6cf
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-10-25 03:38:58 +0600

    Update somecodesonpython.py

--- somecodesonpython.py
+++ somecodesonpython.py
@@ -43,3 +43,19 @@ def Function_sum():
 Function_sum()
 a,b=(31,68)
 Function_sum()
+
+
+#i use some general function here,look bellow
+
+def summ(num1,num2):
+    sum=num1+num2
+    print(sum)
+summ(45,65)#output will=110
+summ(45,5832409)#output will=5832454
+print("--------")
+def sammm(digit1=20,digit2=45):#here 20 and 45 amout is set as
+    summ=digit1+digit2         #defalt!if we donot give any input
+    print(summ)                #then the compiler will automically set it
+
+sammm(65345,28372)#here the defalt value doesnt need to work
+sammm()#here the defalt value will work

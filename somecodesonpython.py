c817e4935ad00dbbec51cc84320f74ee1fd3cc87
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-10-25 02:51:47 +0600

    Create somecodesonpython.py

---
+++ somecodesonpython.py
@@ -0,0 +1,15 @@
+TEXT="Two ans two makes "
+math=2+2
+result=str(math)
+print(TEXT +result)
+
+#another code
+
+x,y,z=(3,4,45)
+print(x+y+z)
+if x+y+z<=45:
+    print("you are not old even now!congratulation")
+elif x+y+z<=35:
+    print("you are even young man")
+else:
+    print("you are old")
